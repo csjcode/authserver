1a4093c14240ff6292e1fa710a44c4eae6eec525
csjcode <christophersstjohn@gmail.com>
2016-08-20 10:48:50 +0700

    Add boolean flag for authentication to reducer (7.98)

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -1,6 +1,9 @@
 export default function (state = {}, action) {
   switch(action.type) {
-
+    case AUTH_USER:
+      return { ...state, authenticated: true };
+    case UNAUTH_USER:
+      return { ...state, authenticated: false };
   }
   return state;
 };
